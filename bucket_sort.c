311ac5946ff3a1e533fa201ee8394b585800d058
asdf <jeffisbestone@gmail.com>
2026-05-06 22:01:50 +0200

    crash out

--- bucket_sort.c
+++ bucket_sort.c
@@ -12,63 +12,78 @@
 
 #include "push_swap.h"
 
+void	mk_stack(t_stack **b_stack, int len)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		add_front(b_stack, new_node(i, i));
+		i++;
+	}
+}
+
+void	get_range(int *min, int *max, t_stack *stack)
+{
+	int	i;
+	int	len;
+
+	if (!stack)
+	{
+		min = 0;
+		max = 0;
+		return ;
+	}
+	*min = *stack->data;
+	*max = *stack->data;
+	len = ft_lstsize(stack);
+	i = 0;
+	while (i < len)
+        {
+                if (*stack->data < *min)
+		       *min = *stack->data;
+                if (*stack->data > *max)
+                        *max = *stack->data;
+		i++;
+                rotate(&stack);
+        }	
+}
+
 void	bucket_sort(t_stack **a_stack, t_stack **b_stack)
 {
 	int		len;
-	int		range[5];
+	int		range[2];
 	t_stack	*sorted;
-	t_stack	*a_top;
-	t_stack	*b_top;
 
 	if (!*a_stack)
 		return ;
-	a_top = *a_stack;
 	len = ft_lstsize(*a_stack);
-	range[0] = *a_top->data;
-	range[1] = *a_top->data;
-	range[2] = 0;
-	while (range[2]++ != len)
-	{
-		if (*a_top->data > range[1])
-			range[1] = *a_top->data;
-		if (*a_top->data < range[0])
-			range[0] = *a_top->data;
-		rotate(&a_top);
-	}
+	get_range(&range[0], &range[1], *a_stack);
 	if (ft_lstsize(*a_stack) == 1 || range[0] == range[1])
 		return ;
-	a_top = *a_stack;
+	mk_stack(b_stack, len);
 	while (*a_stack)
 	{
-		b_top = *b_stack;
-		range[2] = *(*a_stack)->data;
-		//b_top = *b_stack;
-		if (!*b_stack)
-		{
-			push_top(b_stack, a_stack);
-			continue ;
-		}
-		range[3] = ft_lstsize(*b_stack);
-		range[4] = 0;
-		while (range[3] != range[4]++ && *b_top->index < ((range[2] - range[0]) / (range[1] - range[0])) * (len - 1))
-			rotate(&b_top);
-		//printf("data: %d, min: %d, max: %d, len: %d\n", range[2], range[0], range[1], len);
-		//printf("%d : %d : %d\n", *(*b_stack)->index, (((range[2] - range[0]) / (range[1] - range[0])) * (len - 1)), *(*a_stack)->data);
-		push_top(&b_top, a_stack);
-		show_list(&b_top);
+		while (*(*b_stack)->index != ((*(*a_stack)->data - range[0]) * (len - 1)) / (range[1] - range[0]))
+			rotate(b_stack);
+		push_top(&(*b_stack)->lst, a_stack);
 	}
-	//show_list(b_stack);
+	while (*(*b_stack)->index != ft_lstsize(*b_stack) - 1)
+		rotate(b_stack);
 	range[0] = 0;
 	while (range[0]++ != len)
 	{
 		sorted = NULL;
-		bucket_sort(&(*b_stack)->lst, &sorted);
+		//get_range(&range[0], &range[1], (*b_stack)->lst);
+		if (ft_lstsize((*b_stack)->lst) > 1/* && range[0] != range[1]*/)
+			bucket_sort(&(*b_stack)->lst, &sorted);
 		while ((*b_stack)->lst)
 		{
-			reverse_rotate(&(*b_stack)->lst);
+			rotate(&(*b_stack)->lst);
 			push_top(a_stack, &(*b_stack)->lst);
 		}
-		reverse_rotate(b_stack);
+		rotate(b_stack);
 	}
 }
 
